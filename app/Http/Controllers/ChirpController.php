7042b951c20bbb69d38f4e7434735dcf46c3d956
irfanmf1998 <irfanmf1998@gmail.com>
2026-07-11 17:05:57 +0700

    Bab 10 - Edit and delete chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -85,7 +85,8 @@ class ChirpController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        // We'll add authorization in lesson 11
+        return view('chirps.edit', compact('chirp'));
     }
 
     /**
@@ -93,7 +94,15 @@ class ChirpController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // Validate
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        // Update
+        $chirp->update($validated);
+
+        return redirect('/')->with('success', 'Chirp updated!');
     }
 
     /**
@@ -101,6 +110,8 @@ class ChirpController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $chirp->delete();
+
+        return redirect('/')->with('success', 'Chirp deleted!');
     }
 }
