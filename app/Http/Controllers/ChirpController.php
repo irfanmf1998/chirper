c61047c870fa87dd521a4e51b8f488f18a4f6144
irfanmf1998 <irfanmf1998@gmail.com>
2026-07-11 12:31:46 +0700

    Bab 9 - Creating And Storing Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -50,7 +50,26 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the request
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+                Rule::unique('chirps')->where(function ($query) use ($user) {
+                return $query->where('user_id', $user->id);
+            })
+        ], [
+             'message.required' => 'Please write something to chirp!',
+             'message.max' => 'Chirps must be 255 characters or less.',
+        ]);
+
+        // Create the chirp (no user for now - we'll add auth later)
+        \App\Models\Chirp::create([
+            'message' => $validated['message'],
+            'user_id' => null, // We'll add authentication in lesson 11
+        ]);
+
+        // Redirect back to the feed
+        return redirect('/')->with('success', 'Your chirp has been posted!');
+        //return redirect('/')->with('success', 'Chirp created!');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 
 Route::get('/', [ChirpController::class, 'index']);
+Route::post('/chirps', [ChirpController::class, 'store']);
 
 //Route::get('/', function () {
     //return view('welcome');
